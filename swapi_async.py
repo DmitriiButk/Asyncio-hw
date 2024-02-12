48f782308b428abdcada5d3fe91a46a87162116a
DmitriiButk <nudumka21@gmail.com>
2024-02-12 14:11:18 +0400

    added docstrings to functions, repaired function get_data.

--- swapi_async.py
+++ swapi_async.py
@@ -1,21 +1,22 @@
+import datetime
 import asyncio
 import aiohttp
-import datetime
 from more_itertools import chunked
 from models import init_db, SwapiPeopleModel, engine, Session
-
 
 MAX_CHUNK = 10
 MAX_COUNT = 85
 
 
 async def get_person(client: aiohttp.ClientSession, person_id: int):
+    """This function is used to get a single person from the Star Wars API."""
     http_response = await client.get(f'https://swapi.dev/api/people/{person_id}')
     json_result = await http_response.json()
     return json_result
 
 
 async def get_data(list_of_urls: list, value: str, client: aiohttp.ClientSession):
+    """This function is used to get data from a list of urls."""
     some_list = []
     for url in list_of_urls:
         response = await client.get(url)
@@ -25,18 +26,21 @@ async def get_data(list_of_urls: list, value: str, client: aiohttp.ClientSession
 
 
 async def insert_to_db(client: aiohttp.ClientSession, list_of_jsons: list):
+    """This function is used to insert data into the database."""
     async with Session() as session:
         for person in list_of_jsons:
             if person != {'detail': 'Not found'}:
-                films = await get_data(list_of_urls=person['films'], value='title', client=client)
-                species = await get_data(list_of_urls=person['species'], value='name', client=client)
-                starships = await get_data(list_of_urls=person['starships'], value='name', client=client)
-                vehicles = await get_data(list_of_urls=person['vehicles'], value='name', client=client)
+                results = await asyncio.gather(
+                    get_data(list_of_urls=person['films'], value='title', client=client),
+                    get_data(list_of_urls=person['species'], value='name', client=client),
+                    get_data(list_of_urls=person['starships'], value='name', client=client),
+                    get_data(list_of_urls=person['vehicles'], value='name', client=client)
+                )
                 person = SwapiPeopleModel(
-                    films=films,
-                    species=species,
-                    starships=starships,
-                    vehicles=vehicles,
+                    films=results[0],
+                    species=results[1],
+                    starships=results[2],
+                    vehicles=results[3],
                     birth_year=person['birth_year'],
                     eye_color=person['eye_color'],
                     gender=person['gender'],
@@ -52,6 +56,12 @@ async def insert_to_db(client: aiohttp.ClientSession, list_of_jsons: list):
 
 
 async def main():
+    """
+    This function initializes the database, creates an aiohttp client session,
+    and then uses a for loop to make multiple requests to the Star Wars API in parallel.
+    The responses are then inserted into the database. Finally,
+    the tasks set is gathered and awaited, and the client session is closed.
+     """
     await init_db()
     client = aiohttp.ClientSession()
     for chunk in chunked(range(1, MAX_COUNT), MAX_CHUNK):
